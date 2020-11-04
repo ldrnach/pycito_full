2cd93f55f02587e645bbb209a39cb288ee506eec
Luke Drnach <ldrnach3@gatech.edu>
2020-11-04 19:23:34 +0000

    Began gaussian process model

---
+++ systems/gaussianprocess.py
@@ -0,0 +1,23 @@
+"""
+gaussianprocess.py: Gaussian Process Regression tools
+
+Luke Drnach
+October 21, 2020
+"""
+
+import numpy as np
+
+class GaussianProcess():
+    def __init__(self):
+        self.data = []
+        
+        pass
+
+    def add_data(self, x, y):
+        pass
+
+    def predict(self, x):
+        pass
+
+    def kernel(self, x1, x2):
+        pass
