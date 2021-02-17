328da84d205a017d511884127d99a4a1b8615cf8
Luke Drnach <ldrnach3@gatech.edu>
2021-02-17 20:22:33 +0000

    refactored blockopt

--- examples/sliding_block/blockopt.py
+++ examples/sliding_block/blockopt.py
@@ -17,65 +17,191 @@ from pydrake.solvers.snopt import SnoptSolver
 import utilities as utils
 #TODO: Check if the ElasticWeight parameter is available in this version of Drake
 
-# Create the block model with the default flat terrain
-plant = Block()
-plant.Finalize()
-# Get the default context
-context = plant.multibody.CreateDefaultContext()
-# Create a Contact Implicit Trajectory Optimization
-trajopt = ContactImplicitDirectTranscription(plant=plant,
-                                            context=context,
-                                            num_time_samples=101,
-                                            maximum_timestep=0.01,
-                                            minimum_timestep=0.01)
-# Add initial and final state constraints
-x0 = np.array([0., 0.5, 0., 0.])
-xf = np.array([5., 0.5, 0., 0.])
-trajopt.add_state_constraint(knotpoint=0, value=x0)    
-trajopt.add_state_constraint(knotpoint=100, value=xf)
-# Set all the timesteps to be equal
-trajopt.add_equal_time_constraints()
-# Add a running cost on the controls
-Q = 10 * np.ones((1,1))
-b = np.zeros((1,))
-trajopt.add_quadratic_running_cost(Q, b, [trajopt.u], name="ControlCost")
-R = np.diag([1,1,1,1])
-trajopt.add_quadratic_running_cost(R, xf, [trajopt.x], name="StateCost")
-# Add a final cost on the total time
-cost = lambda h: np.sum(h)
-trajopt.add_final_cost(cost, vars=[trajopt.h], name="TotalTime")
-# Set the initial trajectory guess
-u_init = np.zeros(trajopt.u.shape)
-x_init = np.zeros(trajopt.x.shape)
-for n in range(0, x_init.shape[0]):
-    x_init[n,:] = np.linspace(start=x0[n], stop=xf[n], num=101)
-l_init = np.zeros(trajopt.l.shape)
-trajopt.set_initial_guess(xtraj=x_init, utraj=u_init, ltraj=l_init)
-# Get the final program, with all costs and constraints
-prog = trajopt.get_program()
-# Set the SNOPT solver options
-prog.SetSolverOption(SnoptSolver().solver_id(), "Iterations Limit", 10000)
-prog.SetSolverOption(SnoptSolver().solver_id(), "Major Feasibility Tolerance", 1e-6)
-prog.SetSolverOption(SnoptSolver().solver_id(), "Major Optimality Tolerance", 1e-6)
-prog.SetSolverOption(SnoptSolver().solver_id(), "Scale Option", 2)
-solver = SnoptSolver()
-# Enable a visualization callback
-trajopt.enable_cost_display(display='all')
-# Check the problem for bugs in the constraints
-if not utils.CheckProgram(prog):
-    quit()
-# Solve the problem
-print("Solving trajectory optimization")
-start = timeit.default_timer()
-result = solver.Solve(prog)
-stop = timeit.default_timer()
-print(f"Elapsed time: {stop-start}")
-utils.printProgramReport(result, prog)
+def run_block_trajopt():
+    trajopt = setup_block_trajopt()
+    # Set the boundary constraints
+    x0, xf = create_boundary_constraints()
+    set_boundary_constraints(trajopt, x0, xf)
+    # Require the timesteps be equal
+    trajopt.add_equal_time_constraints()
+    # Set the costs
+    add_control_cost(trajopt)
+    add_state_cost(trajopt, xf)
+    add_final_cost(trajopt)
+    # Set the initial condition
+    set_linear_guess(trajopt, x0, xf)
+    # Set the default solver options
+    set_default_snopt_options(trajopt)
+    #Check the problem for bugs in the constraints
+    if not utils.CheckProgram(trajopt.prog):
+        quit()
+    # Solve the problem
+    result = solve_block_trajopt(trajopt)
+    soln = trajopt.result_to_dict(result)
+    # Plot results
+    plot_block_trajectories(trajopt, result)
+    # Save
+    save_block_trajectories(soln, 'block_trajopt.pkl')
+    # Tighten snopt options
+    set_tight_snopt_options(trajopt)
+    initialize_from_previous(trajopt, soln)
+    # Run optimization
+    result = solve_block_trajopt(trajopt)
+    soln = trajopt.result_to_dict(result)
+    # Plot results
+    plot_block_trajectories(trajopt, result)
+    # Save
+    save_block_trajectories(soln, 'block_trajopt_tight.pkl')
 
-# Unpack and plot the trajectories
-xtraj, utraj, ftraj, _ = trajopt.reconstruct_all_trajectories(result)
-plant.plot_trajectories(xtraj, utraj, ftraj)
-print('Done!')
+def setup_block_trajopt():
+    """ Create block plant and contact-implicit trajectory optimization"""
+    plant = Block()
+    plant.Finalize()
+    # Get the default context
+    context = plant.multibody.CreateDefaultContext()
+    # Create a Contact Implicit Trajectory Optimization
+    trajopt = ContactImplicitDirectTranscription(plant=plant,
+                                                context=context,
+                                                num_time_samples=101,
+                                                maximum_timestep=0.01,
+                                                minimum_timestep=0.01)
+    return trajopt
+
+def create_boundary_constraints():
+    x0 = np.array([0., 0.5, 0., 0.])
+    xf = np.array([5., 0.5, 0., 0.])
+    return x0, xf
+
+def set_boundary_constraints(trajopt, x0, xf):
+    trajopt.add_state_constraint(knotpoint=0, value=x0)    
+    trajopt.add_state_constraint(knotpoint=trajopt.num_time_samples-1, value=xf)
+
+def set_linear_guess(trajopt, x0, xf):
+    """
+    Set a linear guess for the initial state trajectory
+    
+    Also sets zero-initialized trajectories for other variables
+    """
+    # Set the initial trajectory guess
+    u_init = np.zeros(trajopt.u.shape)
+    x_init = np.zeros(trajopt.x.shape)
+    for n in range(0, x_init.shape[0]):
+        x_init[n,:] = np.linspace(start=x0[n], stop=xf[n], num=101)
+    l_init = np.zeros(trajopt.l.shape)
+    # Set the guess in the trajopt
+    trajopt.set_initial_guess(xtraj=x_init, utraj=u_init, ltraj=l_init)
+
+def add_control_cost(trajopt):
+    """Add a quadratic cost on the control effort"""
+    nU = trajopt.u.shape[0]
+    R = 10 * np.eye(nU)
+    b = np.zeros((nU,))
+    trajopt.add_quadratic_running_cost(R, b, [trajopt.u], name="ControlCost")
+
+def add_state_cost(trajopt, b):
+    """Add a quadratic cost of the state deviation"""
+    nX = trajopt.x.shape[0]
+    R = np.eye(nX)
+    trajopt.add_quadratic_running_cost(R, b, [trajopt.x], name="StateCost")
+
+def add_final_cost(trajopt):
+    """Add a final cost on the total time for the motion"""
+    cost = lambda h: np.sum(h)
+    trajopt.add_final_cost(cost, vars=[trajopt.h], name="TotalTime")
+
+def set_default_snopt_options(trajopt):
+    """ Set SNOPT solver options """
+    trajopt.prog.SetSolverOption(SnoptSolver().solver_id(), "Iterations Limit", 10000)
+    trajopt.prog.SetSolverOption(SnoptSolver().solver_id(), "Major Feasibility Tolerance", 1e-6)
+    trajopt.prog.SetSolverOption(SnoptSolver().solver_id(), "Major Optimality Tolerance", 1e-6)
+    trajopt.prog.SetSolverOption(SnoptSolver().solver_id(), "Scale Option", 2)
+
+def set_tight_snopt_options(trajopt):
+    trajopt.prog.SetSolverOption(SnoptSolver().solver_id(), "Major Feasibility Tolerance", 1e-8)
+
+def initialize_from_previous(trajopt, soln):
+    trajopt.set_initial_guess(xtraj=soln['state'], utraj=soln['control'], ltraj=soln['force'])
+
+def solve_block_trajopt(trajopt):
+    solver = SnoptSolver()
+    prog = trajopt.get_program()
+    # Solve the problem
+    print("Solving trajectory optimization")
+    start = timeit.default_timer()
+    result = solver.Solve(prog)
+    stop = timeit.default_timer()
+    print(f"Elapsed time: {stop-start}")
+    utils.printProgramReport(result, prog)
+    return result
+
+def plot_block_trajectories(trajopt, result):
+    xtraj, utraj, ftraj, _ = trajopt.reconstruct_all_trajectories(result)
+    trajopt.plant_f.plot_trajectories(xtraj, utraj, ftraj)
+
+def save_block_trajectories(soln, name="block_trajopt.pkl"):
+    file = "data/slidingblock/" + name
+    utils.save(file, soln)
+
+if __name__ == "__main__":
+    run_block_trajopt()
+
+# # Create the block model with the default flat terrain
+# plant = Block()
+# plant.Finalize()
+# # Get the default context
+# context = plant.multibody.CreateDefaultContext()
+# # Create a Contact Implicit Trajectory Optimization
+# trajopt = ContactImplicitDirectTranscription(plant=plant,
+#                                             context=context,
+#                                             num_time_samples=101,
+#                                             maximum_timestep=0.01,
+#                                             minimum_timestep=0.01)
+# # Add initial and final state constraints
+# x0 = np.array([0., 0.5, 0., 0.])
+# xf = np.array([5., 0.5, 0., 0.])
+# trajopt.add_state_constraint(knotpoint=0, value=x0)    
+# trajopt.add_state_constraint(knotpoint=100, value=xf)
+# # Set all the timesteps to be equal
+# trajopt.add_equal_time_constraints()
+# # Add a running cost on the controls
+# Q = 10 * np.ones((1,1))
+# b = np.zeros((1,))
+# trajopt.add_quadratic_running_cost(Q, b, [trajopt.u], name="ControlCost")
+# R = np.diag([1,1,1,1])
+# trajopt.add_quadratic_running_cost(R, xf, [trajopt.x], name="StateCost")
+# # Add a final cost on the total time
+# cost = lambda h: np.sum(h)
+# trajopt.add_final_cost(cost, vars=[trajopt.h], name="TotalTime")
+# # Set the initial trajectory guess
+# u_init = np.zeros(trajopt.u.shape)
+# x_init = np.zeros(trajopt.x.shape)
+# for n in range(0, x_init.shape[0]):
+#     x_init[n,:] = np.linspace(start=x0[n], stop=xf[n], num=101)
+# l_init = np.zeros(trajopt.l.shape)
+# trajopt.set_initial_guess(xtraj=x_init, utraj=u_init, ltraj=l_init)
+# # Get the final program, with all costs and constraints
+# prog = trajopt.get_program()
+# # Set the SNOPT solver options
+# prog.SetSolverOption(SnoptSolver().solver_id(), "Iterations Limit", 10000)
+# prog.SetSolverOption(SnoptSolver().solver_id(), "Major Feasibility Tolerance", 1e-6)
+# prog.SetSolverOption(SnoptSolver().solver_id(), "Major Optimality Tolerance", 1e-6)
+# prog.SetSolverOption(SnoptSolver().solver_id(), "Scale Option", 2)
+# solver = SnoptSolver()
+# # Enable a visualization callback
+# trajopt.enable_cost_display(display='all')
+# #
+# # Solve the problem
+# print("Solving trajectory optimization")
+# start = timeit.default_timer()
+# result = solver.Solve(prog)
+# stop = timeit.default_timer()
+# print(f"Elapsed time: {stop-start}")
+# utils.printProgramReport(result, prog)
+
+# # Unpack and plot the trajectories
+# xtraj, utraj, ftraj, _ = trajopt.reconstruct_all_trajectories(result)
+# plant.plot_trajectories(xtraj, utraj, ftraj)
+# print('Done!')
 
 # Save the results
 # file = "data/slidingblock/block_trajopt.pkl"
