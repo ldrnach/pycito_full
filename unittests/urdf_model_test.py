9bedae191d2f2641ee6af271d981ce0ba0f4d342
Luke Drnach <ldrnach3@gatech.edu>
2021-04-22 14:55:26 +0000

    updated unit test urdf_model_test.py

--- unittests/urdf_model_test.py
+++ unittests/urdf_model_test.py
@@ -7,17 +7,15 @@ October 14, 2020
 
 import numpy as np
 import unittest
-from math import pi, sin, cos
 from utilities import FindResource
-from pydrake.all import MultibodyPlant, RigidTransform, JacobianWrtVariable, MultibodyForces
-from pydrake.multibody.parsing import Parser
+from pydrake.all import Parser, MultibodyPlant, RigidTransform, JacobianWrtVariable, MultibodyForces
 
 class TestSlidingBlock(unittest.TestCase):
     
     @classmethod
     def setUpClass(cls):
         """Find and load the sliding_block urdf file"""
-        urdf_file = FindResource("systems/urdf/sliding_block.urdf")
+        urdf_file = FindResource("systems/block/urdf/sliding_block.urdf")
         cls.plant = MultibodyPlant(0.0)
         cls.block = Parser(cls.plant).AddModelFromFile(urdf_file)
         body_inds = cls.plant.GetBodyIndices(cls.block)
@@ -29,16 +27,16 @@ class TestSlidingBlock(unittest.TestCase):
     def setUp(self):
         """Create a context variable for each test"""
         self.context = self.plant.CreateDefaultContext()
-        x = [0,3, pi/4, 1, -0.5, 0.01]
+        x = [0,3, 1, -0.5]
         self.plant.SetPositionsAndVelocities(self.context, x)
 
     def test_num_positions(self):
         """Check the number of position variables in the model"""
-        self.assertEqual(self.plant.num_positions(), 3, msg="Wrong number of position variables")
+        self.assertEqual(self.plant.num_positions(), 2, msg="Wrong number of position variables")
 
     def test_num_velocities(self):
         """Check the number of velocity variables in the model"""
-        self.assertEqual(self.plant.num_velocities(), 3, msg="Wrong number of velocity variables")
+        self.assertEqual(self.plant.num_velocities(), 2, msg="Wrong number of velocity variables")
 
     def test_num_actuators(self):
         """Check the number of actuators in the model"""
@@ -47,32 +45,32 @@ class TestSlidingBlock(unittest.TestCase):
     def test_mass_matrix(self):
         """ Check the value of the mass matrix"""
         M = self.plant.CalcMassMatrixViaInverseDynamics(self.context)
-        M_true = np.diag([1., 1., 0.167])
+        M_true = np.diag([1., 1.])
         self.assertTrue(np.allclose(M,M_true,), msg="Mass matrix is incorrect")
 
     def test_bias_term(self):
         """Check the value of the Coriolis and centrifugal terms"""
         C = self.plant.CalcBiasTerm(self.context)
-        C_true = np.zeros((3,))
+        C_true = np.zeros((2,))
         self.assertTrue(np.allclose(C, C_true), msg="Bias term is incorrect")
 
     def test_grav_term(self):
         """Check the value of gravitational and conservative forces"""
         G = self.plant.CalcGravityGeneralizedForces(self.context)
-        G_true = np.zeros((3,))
+        G_true = np.zeros((2,))
         G_true[1] = -9.81
         self.assertTrue(np.allclose(G,G_true), msg="Gravity terms is incorrect")
 
     def test_actuation_matrix(self):
         """Test the actuation matrix"""
         B = self.plant.MakeActuationMatrix()
-        B_true = np.array([[1.], [0.], [0.]])
+        B_true = np.array([[1.], [0.]])
         self.assertTrue(np.allclose(B, B_true), msg="Actuation matrix is incorrrect")
 
     def test_inverse_dynamics(self):
         """Check the value returned by inverse dynamics"""
-        dv = np.array([0.1, -0.1, 0.2])
-        m = np.array([1, 1, 0.167])
+        dv = np.array([0.1, -0.1])
+        m = np.array([1, 1])
         # Note that inverse dynamics does not account for gravity
         tau_true = m * dv
         mbf = MultibodyForces(self.plant)
@@ -81,11 +79,11 @@ class TestSlidingBlock(unittest.TestCase):
 
     def test_center_jacobian(self):
         """Check the value returned by the Jacobian"""
-        p = np.array([0., 0., -0.5])
+        p = np.array([0., 0., 0.])
         q = self.plant.GetPositions(self.context)
-        Jp_true = np.array([[1.0, 0.0, -0.5*cos(q[2])],
-                            [0.0, 0.0,  0.0],
-                            [0.0, 1.0,  0.5*sin(q[2])]])
+        Jp_true = np.array([[1.0, 0.0],
+                            [0.0, 0.0],
+                            [0.0, 1.0]])
         base_frame = self.plant.GetBodyByName('box').body_frame()
         wframe = self.plant.world_frame()
         Jp = self.plant.CalcJacobianTranslationalVelocity(self.context, JacobianWrtVariable.kQDot, base_frame, p, wframe, wframe)
