1cdde1f3c32f696b56862192d1c99fab4db7e3ec
Luke Drnach <ldrnach3@gatech.edu>
2021-04-30 15:56:41 +0000

    tested quaternion functions in trajopt/quatutils.py

--- trajopt/quatutils.py
+++ trajopt/quatutils.py
@@ -4,7 +4,6 @@ quaternion utilities
 Luke Drnach
 April 16, 2021
 """
-
 import numpy as np
 
 def quaternion_product(q1, q2):
@@ -17,7 +16,7 @@ def quaternion_product(q1, q2):
     """
     return np.hstack([
         q1[0]*q2[0] - np.dot(q1[1:], q2[1:]),
-        q1[0] * q2[1:] + q2[0]*q1[1:] - np.cross(q1[1:], q2[1:])
+        q1[0] * q2[1:] + q2[0]*q1[1:] + np.cross(q1[1:], q2[1:])
     ])
 
 def rpy_rates_to_velocity(rpy, rates):
@@ -41,7 +40,7 @@ def xyz_to_quaternion(xyz):
     qx = np.array([np.cos(x/2), np.sin(x/2), 0., 0.])
     qy = np.array([np.cos(y/2), 0., np.sin(y/2), 0.])
     qz = np.array([np.cos(z/2), 0., 0., np.sin(z/2)])
-    return quaternion_product(qx, quaternion_product(qy, qz))
+    return quaternion_product(qz, quaternion_product(qy, qx))
 
 def quaternion_to_xyz(quat):
     """ 
@@ -50,13 +49,35 @@ def quaternion_to_xyz(quat):
     if quat.ndim == 1:
         quat = np.expand_dims(quat, axis=1)
     xyz = np.zeros((3, quat.shape[1]), dtype=quat.dtype)
-    xyz[0,:] = np.arctan2(2*(quat[2,:]*quat[3,:] + quat[0,:]*quat[1,:]), quat[0,:]**2 + quat[1,:]**2 - quat[2,:]**2 - quat[3,:]**2)
-    xyz[1,:] = -np.arcsin(2*(quat[3,:]*quat[1,:] - quat[0,:]* quat[2,:]))
-    xyz[2,:] = np.arctan2(2*(quat[3,:]*quat[1,:] - quat[0,:]*quat[2,:]))
+    # Rotation matrix elements
+    r11 = quat[0,:]**2 + quat[1,:]**2 - quat[2,:]**2 -quat[3,:]**2
+    r21 = 2*(quat[1,:]*quat[2,:] + quat[0,:]*quat[3,:])
+    r31 = 2*(quat[3,:]*quat[1,:] - quat[0,:]*quat[2,:])
+    r32 = 2*(quat[2,:]*quat[3,:] + quat[0,:]*quat[1,:])
+    r33 = quat[0,:]**2 - quat[1,:]**2 - quat[2,:]**2 + quat[3,:]**2
+    # Calculate the Y-Axis rotation first
+    xyz[1,:] = np.arctan2(-r31, np.sqrt(r11**2 + r21**2))
+    s = np.sign(np.cos(xyz[1,:]))   # Check the sign 
+    s[s==0] = 1.
+    # Calculate X and Z rotations
+    xyz[0,:] = np.arctan2(r32/s, r33/s)
+    xyz[2,:] = np.arctan2(r21/s, r11/s)
     if xyz.shape[1] == 1:
         return np.squeeze(xyz)
     else:
         return xyz
+
+def quaternion_to_rpy(quat):
+    """ 
+    Convert a quaternion to Roll-Pitch-Yaw
+    
+    Arguments:
+        quaternion: a (4,n) numpy array of quaternions
+    
+    Return values:
+        rpy: a (3,n) numpy array of roll-pitch-yaw values
+    """
+    return quat2rpy(quat)
 
 def quat2rpy(quat):
     """
@@ -68,13 +89,26 @@ def quat2rpy(quat):
     Return values:
         rpy: a (3,n) numpy array of roll-pitch-yaw values
     """
+    if quat.ndim == 1:
+        quat = np.expand_dims(quat, axis=1)
     rpy = np.zeros((3, quat.shape[1]))
-    rpy[0,:] = np.arctan2(2*(quat[0,:]*quat[1,:] + quat[2,:]*quat[3,:]),
-                         1-2*(quat[1,:]**2 + quat[2,:]**2))
-    rpy[1,:] = np.arcsin(2*(quat[0,:]*quat[2,:]-quat[3,:]*quat[1,:]))
-    rpy[2,:] = np.arctan2(2*(quat[0,:]*quat[3,:]+quat[1,:]*quat[2,:]),
-                        1-2*(quat[2,:]**2 + quat[3,:]**2))
-    return rpy
+    # Rotation matrix components
+    r11 = quat[0,:]**2 + quat[1,:]**2 - quat[2,:]**2 -quat[3,:]**2
+    r12 = 2*(quat[1,:]*quat[2,:] - quat[0,:]*quat[3,:])
+    r13 = 2*(quat[3,:]*quat[1,:] + quat[0,:]*quat[2,:])
+    r23 = 2*(quat[2,:]*quat[3,:] - quat[0,:]*quat[1,:])
+    r33 = quat[0,:]**2 - quat[1,:]**2 - quat[2,:]**2 + quat[3,:]**2
+    # Get the pitch angle first
+    rpy[1,:] = np.arctan2(r13, np.sqrt(r11**2 + r12**2))
+    s = np.sign(rpy[1,:])
+    s[s==0] = 1.
+    # Get roll and yaw angles
+    rpy[0,:] = np.arctan2(-r23/s, r33/s)
+    rpy[2,:] = np.arctan2(-r12/s, r11/s)
+    if quat.shape[1] == 1:
+        return np.squeeze(rpy)
+    else:
+        return rpy
 
 def rpy_to_quaternion(rpy):
     """
@@ -89,7 +123,7 @@ def rpy_to_quaternion(rpy):
     qx = np.array([np.cos(r/2), np.sin(r/2), 0, 0])
     qy = np.array([np.cos(p/2), 0, np.sin(p/2), 0])
     qz = np.array([np.cos(y/2), 0, 0, np.sin(y/2)])
-    return quaternion_product(qz, quaternion_product(qy, qx))
+    return quaternion_product(qx, quaternion_product(qy, qz))
 
 def integrate_quaternion(q, w_axis, w_mag, dt):
     """
