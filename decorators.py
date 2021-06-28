f4c2fbef0b3a4db73500cb289a391037f9ff40d1
Luke Drnach <ldrnach3@gatech.edu>
2021-06-28 20:29:05 +0000

    added saveable_fig and showable_fig to decorators.py

--- decorators.py
+++ decorators.py
@@ -6,6 +6,9 @@ June 18, 2021
 """
 import timeit
 import functools
+from matplotlib import pyplot as plt
+
+#TODO: Figure out why showable_fig doesn't keep the plots open
 
 def timer(func):
     """Print the runtime of the decorated function. The decorator also records the time in the total_time attribute"""
@@ -19,3 +22,35 @@ def timer(func):
         print(f"Finished {func.__name__!r} in {wrapper_timer.total_time:.4f} seconds")
         return value
     return wrapper_timer
+
+def saveable_fig(func):
+    """Save a figure from the output"""
+    @functools.wraps(func)
+    def wrapper_saveable_fig(*args, **kwargs):
+        fig, axs = func(*args, **kwargs)
+        # Check kwargs for a savename and save the figure
+        if 'savename' in kwargs and kwargs['savename'] is not None:
+            fig.savefig(kwargs['savename'], dpi=fig.dpi)
+        return fig, axs
+    return wrapper_saveable_fig
+
+def showable_fig(func):
+    """Show a figure based on a show argument"""
+    @functools.wraps(func)
+    def wrapper_showable_fig(*args, **kwargs):
+        fig, axs = func(*args, **kwargs)
+        # Check kwargs for a show key and show the figure
+        if 'show' in kwargs and kwargs['show']:
+            plt.show(block=True)
+        return fig, axs
+    return wrapper_showable_fig
+
+# @saveable_fig
+# @showable_fig
+# def testfig(show=False, savename=None):
+#     fig, axs = plt.subplots(1,1)
+#     axs.plot(0, 0)
+#     return fig, axs
+
+# if __name__ == '__main__':
+#     testfig(show=True, savename=None)
