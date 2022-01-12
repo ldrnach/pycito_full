923ee49da82be64106d3e1067d7f62139d173e6d
Luke Drnach <ldrnach3@gatech.edu>
2022-01-12 15:23:51 -0500

    added setup.py to make it a local package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name = 'pycito',
+    version = '2.0',
+    packages = find_packages(),
+    description='contact implicit trajectory optimization in python',
+    url='https://github.gatech.edu/GeorgiaTechLIDARGroup/pyCITO',
+    author='Luke Drnach',
+    author_email='luke.drnach@gatech.edu',
+)
