145422372e5f4c211cf5ec610a3a64a3095eaddb
Luke Drnach <ldrnach3@gatech.edu>
2022-02-14 17:40:20 -0500

    fixed bugs in concatenating a1 trajectories

--- examples/a1/combine_a1_gaitsteps.py
+++ examples/a1/combine_a1_gaitsteps.py
@@ -1,5 +1,5 @@
 
-import os
+import os, copy
 import numpy as np
 
 from pydrake.all import PiecewisePolynomial as pp
@@ -8,8 +8,6 @@ import pycito.utilities as utils
 from pycito.systems.A1.a1 import A1VirtualBase
 
 keys = ['state','control','force','slacks','jointlimit']
-
-
 
 def concatenate_data(dataset):
     alldata = dataset.pop(0)
@@ -20,13 +18,40 @@ def concatenate_data(dataset):
             alldata[key] = np.concatenate([alldata[key], data[key][:, 1:]], axis=1)
     return alldata
 
+def join_backward_euler_trajectories(dataset):
+    """
+    Join trajectories, ensuring dynamic consistency with respect to Backward Euler Dynamics
+    """
+    fulldata = dataset.pop(0)
+    for data in dataset:
+        # First, increment time appropriately
+        t_new = data['time'][1:] + fulldata['time'][-1]
+        fulldata['time'] = np.concatenate([fulldata['time'], t_new], axis=0)
+        # Next, reset the base positions - translate the base
+        data['state'][:3, :] += fulldata['state'][:3, -1:] - data['state'][:3, :1]
+        # Join the state data
+        fulldata['state'] = np.concatenate([fulldata['state'], data['state'][:, 1:]], axis=1)
+        # Join the control torques - use the first control from the next segment
+        fulldata['control'] = np.concatenate([fulldata['control'][:, :-1], data['control']], axis=1)
+        # Join the reaction and joint limit forces appropriately - use the last force set from the previous segment
+        fulldata['force'] = np.concatenate([fulldata['force'], data['force'][:, 1:]], axis=1)
+        fulldata['jointlimit'] = np.concatenate([fulldata['jointlimit'], data['jointlimit'][:, 1:]], axis=1)
+        # Join the slack trajectories - join as with the states
+        fulldata['slacks'] = np.concatenate([fulldata['slacks'], data['slacks'][:, 1:]], axis=1)
+    
+    return fulldata
+
+
+
 def main(numsteps=1):
     dir = os.path.join('examples','a1','foot_tracking_gait')
     subdirs = ['first_step','second_step']
     filepart = os.path.join('weight_1e+03', 'trajoptresults.pkl')
     data = [utils.load(utils.FindResource(os.path.join(dir, subdir, filepart))) for subdir in subdirs]
-    data = data * numsteps
-    data = concatenate_data(data)
+    copy_data = copy.deepcopy(data)
+    for _ in range(numsteps-1):
+        data.extend(copy.deepcopy(copy_data))
+    data = join_backward_euler_trajectories(data)
     trajdata = {}
     trajdata['state'] = pp.FirstOrderHold(data['time'], data['state'])
     for key in keys[1:]:
@@ -34,7 +59,7 @@ def main(numsteps=1):
     # Plot the data
     a1 = A1VirtualBase()
     a1.Finalize()
-    savedir = os.path.join(dir, f'{2*numsteps}step_plots_discontinuous')
+    savedir = os.path.join(dir, f'{2*numsteps}step_plots')
     if not os.path.isdir(savedir):
         os.makedirs(savedir)
     a1.plot_trajectories(trajdata['state'], trajdata['control'], trajdata['force'], trajdata['jointlimit'], show=False, savename=os.path.join(savedir, 'vis.png'))
@@ -43,4 +68,4 @@ def main(numsteps=1):
 
 
 if __name__ == "__main__":
-    main(numsteps=1)
+    main(numsteps=3)
