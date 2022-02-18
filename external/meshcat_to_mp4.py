b94785316bb1aae8de619d8218e1f9d5d0aba0ed
Luke Drnach <ldrnach3@gatech.edu>
2022-02-18 10:31:05 -0500

    minor a1 trajopt changes to fast_gait

--- external/meshcat_to_mp4.py
+++ external/meshcat_to_mp4.py
@@ -99,7 +99,7 @@ def main(args):
     batch_convert_files(infile, outfile)
 
 if __name__ == "__main__":
-    infile = "/home/ldrnach3/Downloads/twostepopt"
-    outfile = "/home/ldrnach3/Projects/drake/pyCITO/examples/a1/foot_tracking_gait/twostepopt"
+    infile = os.path.join('/home','ldrnach3','Downloads','foot_tracking_fast')
+    outfile = os.path.join('/home','ldrnach3','Projects','drake','pyCITO','examples','a1','foot_tracking_fast') 
     batch_convert_files(infile, outfile)
     #main(sys.argv[1:])
