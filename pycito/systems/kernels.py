84c38ab5f858a27aebe315ea179f0d3630e7f6e9
Luke Drnach <ldrnach3@gatech.edu>
2022-02-25 12:24:36 -0500

    refactored kernels, more unittests

--- pycito/systems/kernels.py
+++ pycito/systems/kernels.py
@@ -4,106 +4,132 @@ kernels: basic implementation of kernel functions
 Luke Drnach
 February 22, 2022
 """
+from difflib import Differ
 import numpy as np
 import abc
 
-class DifferentiableKernelBase(abc.ABC):
+class DifferentiableStationaryKernel(abc.ABC):
     
     def __call__(self, X, Y = None):
+        """
+        Evaluate the kernel matrix
+        """
         if Y is None:
-            return self._symmetric_kernel_matrix(X)
+            return self.eval(X, X)
         else:
-            return self._kernel_matrix(X, Y)
+            return self.eval(X, Y)
 
-    def _kernel_matrix(self, X, Y):
+    @staticmethod
+    def _reshape_inputs(X, Y):
+        return np.reshape(X, (X.shape[0], -1)), np.reshape(Y, (Y.shape[0], -1))
+
+    @staticmethod
+    def _squared_distance(X, Y):
         """
-        Arguments:
-            X: (n_features, n_samples_x) set of feature vectors
-            Y: (n_features, n_samples_y) set of feature vectors
+        Calculate the squared distance between the two arrays
         
-        Return values:
-            K: (n_samples_x, n_samples_y) kernel matrix
-        """
-        # Input checking
-        assert X.shape[0] == Y.shape[0], 'X and Y must have the same number of rows (features)'
-        X = np.reshape(X, (X.shape[0], -1)).T   # broadcasting for (N, ) vectors
-        Y = np.reshape(Y, (Y.shape[0], -1)).T
-        if X.dtype is np.dtype(object) or Y.dtype is np.dtype(object):
-            ktype=np.dtype(object)
-        else:
-            ktype=np.dtype(float)
-        # Create the kernel matrix
-        K = np.zeros((X.shape[0], Y.shape[0]), dtype=ktype)
-        for i, x in enumerate(X):
-            for j, y in enumerate(Y):
-                K[i, j] = self.eval(x, y)
-        return K
-
-    def _symmetric_kernel_matrix(self, X):
-        """
         Arguments:
-            X: (n_features, n_samples_x) set of feature vectors
-        Return values:
-            K: (n_samples_x, n_samples_x) symmetric positive-semidefinite kernel matrix
+            x: (n_features, n_samples_x) array of exapmle vectors
+            y: (n_features, n_samples_y) array of example vectors
+
+        Return:
+            D: (n_samples_x, n_samples_y) array of squared distance values
         """
-        # Input checking
-        X = np.reshape(X, (X.shape[0], -1)).T   #Broadcasting for (N,) vectors
-        if X.dtype is np.dtype(object):
-            ktype=np.dtype(object)
-        else:
-            ktype=np.dtype(float)
-        # Calculate the kernel matrix
-        K = np.zeros((X.shape[0], X.shape[0]), dtype=ktype)
-        for i, x in enumerate(X):
-            K[i, i] = 0.5 * self.eval(x, x)
-            for j in range(i+1, X.shape[0]):
-                K[i, j] = self.eval(x, X[j,:])
-        # Symmetrize
-        return K + K.transpose()
+        assert X.shape[0] == Y.shape[0], f'example vectors x and y must have the same first dimension'
+        X, Y = DifferentiableStationaryKernel._reshape_inputs(X, Y)
+        return np.sum((X.T[:, None] - Y.T)**2, axis=-1)
+
+    def eval(self, x, y):
+        """
+        Return the kernel matrix calculated from two example datapoints
+    
+        Arguments:
+            x: (n_features, n_samples_x) array of example vectors
+            y: (n_features, n_samples_y) array of example vectors
+        
+        Returns:
+            K: (n_samples_x, n_samples_y) array of kernel values
+        """
+        D = self._squared_distance(x, y)
+        return self._eval_stationary(D)
 
     @abc.abstractmethod
-    def eval(self, x, y):
+    def _eval_stationary(self, sq_dist):
         raise NotImplementedError
 
     @abc.abstractmethod
     def gradient(self, x, y):
         raise NotImplementedError
 
-class RBFKernel(DifferentiableKernelBase):
+class RBFKernel(DifferentiableStationaryKernel):
     def __init__(self, length_scale = 1.0):
         assert length_scale > 0, "length_scale must be positive"
         self.length_scale = 1.0
         self._scale = -1/(2*self.length_scale **2)
 
-    def eval(self, x, y):
-        """Evaluate the kernel"""
-        return np.exp(self._scale * (x - y).dot(x - y))
+    def _eval_stationary(self, distances):
+        """
+        Evaluate the kernel, using a matrix of pointwise distances
+        
+        Arguments:
+            distances: (N, M) array of pointwise distances
+        
+        Return Values:
+            (N, M) array of kernel values
+        """
+        return np.exp(self._scale * distances)
 
     def gradient(self, x, y):
-        """Evaluate the gradient of the kernel"""
+        """
+        Evaluate the gradient of the kernel
+        
+        Arguments:
+            x: (n_features, 1) array. This is argument for which the gradient is calculated
+            y: (n_features, n_samples_y) array. This is an example point
+        
+        Return values:
+            grad: (n_samples_y, n_features) array of gradients with respect to the input x
+        """
+        x, y = self._reshape_inputs(x, y)
         K = self.eval(x, y)
-        return 2 * self._scale * K * (x - y).transpose()
+        assert K.shape[0] == 1, f"Gradient calculation supports only a single vector as first input"
+        return 2 * self._scale * np.diag(K.flatten()).dot((x - y).transpose())
 
-class PseudoHuberKernel(DifferentiableKernelBase):
+class PseudoHuberKernel(DifferentiableStationaryKernel):
     def __init__(self, length_scale=1.0, delta=1.0):
         assert length_scale > 0, 'length_scale must be positive'
         assert delta > 0, 'delta must be positive'
         self._length_scale =  length_scale
         self._delta = delta
 
-    def _pseudohuber(self, x, y): 
-        return np.sqrt(1 + (x - y).dot(x-y)/self._delta ** 2 )
+    def _pseudohuber(self, dist): 
+        return np.sqrt(1 + dist/self._delta ** 2 )
 
-    def eval(self, x, y):
-        """Evaluate the kernel"""
-        p = self._pseudohuber(x, y)
-        return np.exp( - self._delta ** 2 / self._length_scale * (p - 1))
+    def _eval_stationary(self, dist):
+        """
+        Evaluate the kernel using the distance between the two sample points
+        """
+        p = self._pseudohuber(dist)
+        return np.exp(-self._delta ** 2 / self._length_scale * (p - 1))
 
     def gradient(self, x, y):
-        """Evaluate the gradient of the kernel"""
-        K = self.eval(x, y)
-        p = self._pseudohuber(x, y)
-        return - 1 /(self._length_scale * p) * K * (x - y).transpose()
+        """
+        Evaluate the gradient of the kernel
+        
+        Arguments:
+            x: (n_features, 1) array. This is argument for which the gradient is calculated
+            y: (n_features, n_samples_y) array. This is an example point
+        
+        Return values:
+            grad: (n_samples_y, n_features) array of gradients with respect to the input x
+        """
+        x, y = self._reshape_inputs(x, y)
+        d = self._squared_distance(x, y)
+        assert d.shape[0] == 1, "gradient calculation supports only a single vector as first input"
+        K = self._eval_stationary(d)
+        p = self._pseudohuber(d)
+        dK = 1/(self._length_scale * p) * K
+        return - np.diag(dK.flatten()).dot((x - y).transpose())
 
 if __name__ == '__main__':
     print("Hello from kernels.py!")
--- pycito/tests/unittests/kernels_test.py
+++ pycito/tests/unittests/kernels_test.py
@@ -4,8 +4,6 @@ unittests for pycito.systems.kernels.py
 Luke Drnach
 February 23, 2022
 """
-#TODO: Handle cases where input to kernel is (N, 1) instead of (N, )
-#TODO: Unittesting using autodiff types as inputs - (N,1) types
 import unittest
 import numpy as np
 import pydrake.autodiffutils as ad
@@ -25,31 +23,63 @@ class RBFKernelTest(unittest.TestCase):
                                             [-13/2, -5/2]]))
 
     def test_eval(self):
+        """Test evaluation of the kernel for float inputs"""
         # test that we can evaluate the kernel for single inputs
         k = self.kernel.eval(self.x[:, 0], self.y[:, 0])
-        np.testing.assert_allclose(k, self.Kxy_expected[0,0], atol=1e-6, err_msg=f"Kernel evaluation inaccurate for single inputs")
+        np.testing.assert_allclose(k, self.Kxy_expected[:1, :1], atol=1e-6, err_msg=f"Kernel evaluation inaccurate for single inputs")
         # test normalization
         kn = self.kernel.eval(self.y[:, 0], self.y[:, 0])
-        np.testing.assert_allclose(kn, np.ones((1,)), atol=1e-6, err_msg=f"Kernel is not normalized")
-
-    def test_gradient(self):
-        # Test that we can evaluate the kernel gradient on single inputs
-        x_ad = np.squeeze(ad.InitializeAutoDiff(self.x[:, 2]))
-        k_ad = self.kernel.eval(x_ad, self.y[:, 0])
-        dk_ad = k_ad.derivatives()
-        # Evaluate the gradient directly
-        dk = self.kernel.gradient(self.x[:, 2], self.y[:, 0])
-        np.testing.assert_allclose(dk, dk_ad, atol=1e-6, err_msg=f"Gradient calculation does not match gradient calcuated by autodiff")
+        np.testing.assert_allclose(kn, np.ones((1,1)), atol=1e-6, err_msg=f"Kernel is not normalized")
+        # Test on a single x vector and multiple y vectors
+        k = self.kernel.eval(self.x[:, 0], self.y)
+        np.testing.assert_allclose(k, self.Kxy_expected[:1, :], atol=1e-6, err_msg="Kernel evaluation fails for a single first input with multiple second inputs")
+        # Test on a multiple x vectors and a single y vector
+        k = self.kernel.eval(self.x, self.y[:, 0])
+        np.testing.assert_allclose(k, self.Kxy_expected[:, :1], atol=1e-6, err_msg=f"Kernel evaluation fails for multiple first input vectors, but a single second input vector")
 
     def test_call_symmetric(self):
         # Test that calling the kernel alone results in a symmetric matrix
         Kxx = self.kernel(self.x)
         np.testing.assert_allclose(Kxx, self.Kxx_expected, atol=1e-6, err_msg=f"Calling kernel on a single dataset results in inaccurate kernel matrix")
 
-    def test_call_asymmetric(self):
-        # Test that calling the kernel on two datasets results in an asymmetric matrix
-        Kxy = self.kernel(self.x, self.y)
-        np.testing.assert_allclose(Kxy, self.Kxy_expected, atol=1e-6, err_msg=f"Calling kernel on two datasets results in inaccurate asymmetric kernel matrix")
+    def test_gradient(self):
+        """Test evaluation of the kernel gradients"""
+        x_ad = np.squeeze(ad.InitializeAutoDiff(self.x[:, 2]))
+        # Test evaluating the kernel gradient with a single y-sample
+        k_ad = self.kernel.eval(x_ad, self.y[:, 0])
+        dk_ad = ad.ExtractGradient(k_ad)
+        dk = self.kernel.gradient(self.x[:, 2], self.y[:, 0])
+        np.testing.assert_allclose(dk, dk_ad, atol=1e-6, err_msg=f"Gradient calculation does not match gradient calculated by autodiff for single y-vector")
+        # Test that we can evaluate the kernel gradient with multiple y-samples
+        k_ad = self.kernel.eval(x_ad, self.y)
+        dk_ad = ad.ExtractGradient(k_ad)
+        # Evaluate the gradient directly
+        dk = self.kernel.gradient(self.x[:, 2], self.y)
+        np.testing.assert_allclose(dk, dk_ad, atol=1e-6, err_msg=f"Gradient calculation does not match gradient calcuated by autodiff for multiple y-vectors")
+
+    def test_eval_autodiff(self):
+        """ Test evaluating the kernel when one of the inputs is an autodiff type"""
+        x_ad = ad.InitializeAutoDiff(self.x[:, 0])
+        # Test with a single sample y-point
+        Kxy_ad = self.kernel.eval(x_ad, self.y[:, 0])
+        np.testing.assert_allclose(ad.ExtractValue(Kxy_ad), self.Kxy_expected[:1, :1], atol=1e-6, err_msg=f"Kernel evaluation fails when first argument is autodiff, second argument is vector")
+        # Test with multiple input sample y-points
+        Kxy_ad = self.kernel.eval(x_ad, self.y)
+        np.testing.assert_allclose(ad.ExtractValue(Kxy_ad), self.Kxy_expected[:1,:], atol=1e-6, err_msg=f"Kernel evaluation fails when first argument is autodiff, second argument is array")
+    
+    def test_gradient_autodiff(self):
+        """Test evaluating the kernel gradient when one of the inputs is an autodiff type"""
+        x_ad = ad.InitializeAutoDiff(self.x[:, 0])
+        # Test with a single y-point
+        dKxy_ad  =self.kernel.gradient(x_ad, self.y[:, 0])
+        dKxy = ad.ExtractValue(dKxy_ad)
+        gradKxy = self.kernel.eval(x_ad, self.y[:, 0])
+        np.testing.assert_allclose(dKxy, ad.ExtractGradient(gradKxy), atol=1e-6, err_msg=f"Evaluating gradient fails when first input is autodiff, second input is a single vector")
+        # Test with multiple y-points
+        dKxy_ad = self.kernel.gradient(x_ad, self.y)
+        dKxy = ad.ExtractValue(dKxy_ad)
+        gradKxy = self.kernel.eval(x_ad, self.y)
+        np.testing.assert_allclose(dKxy, ad.ExtractGradient(gradKxy), atol=1e-6, err_msg=f"Evaluating gradient fails when first input is autodiff, second input is multiple vectors")
 
 class PsuedoHuberKernelTest(unittest.TestCase):
     def setUp(self):
@@ -62,21 +92,19 @@ class PsuedoHuberKernelTest(unittest.TestCase):
         self.Kxx_expected = np.exp(1 - np.sqrt(dxx))
 
     def test_eval(self):
+        """Test evaluation of the kernel for float inputs"""
         # test that we can evaluate the kernel for single inputs
         k = self.kernel.eval(self.x[:, 0], self.y[:, 0])
-        np.testing.assert_allclose(k, self.Kxy_expected[0,0], atol=1e-6, err_msg=f"Kernel evaluation inaccurate for single inputs")
+        np.testing.assert_allclose(k, self.Kxy_expected[:1, :1], atol=1e-6, err_msg=f"Kernel evaluation inaccurate for single inputs")
         # test normalization
         kn = self.kernel.eval(self.y[:, 0], self.y[:, 0])
-        np.testing.assert_allclose(kn, np.ones((1,)), atol=1e-6, err_msg=f"Kernel is not normalized")
-
-    def test_gradient(self):
-        # Test that we can evaluate the kernel gradient on single inputs
-        x_ad = np.squeeze(ad.InitializeAutoDiff(self.x[:, 2]))
-        k_ad = self.kernel.eval(x_ad, self.y[:, 0])
-        dk_ad = k_ad.derivatives()
-        # Evaluate the gradient directly
-        dk = self.kernel.gradient(self.x[:, 2], self.y[:, 0])
-        np.testing.assert_allclose(dk, dk_ad, atol=1e-6, err_msg=f"Gradient calculation does not match gradient calcuated by autodiff")
+        np.testing.assert_allclose(kn, np.ones((1,1)), atol=1e-6, err_msg=f"Kernel is not normalized")
+        # Test on a single x vector and multiple y vectors
+        k = self.kernel.eval(self.x[:, 0], self.y)
+        np.testing.assert_allclose(k, self.Kxy_expected[:1, :], atol=1e-6, err_msg="Kernel evaluation fails for a single first input with multiple second inputs")
+        # Test on a multiple x vectors and a single y vector
+        k = self.kernel.eval(self.x, self.y[:, 0])
+        np.testing.assert_allclose(k, self.Kxy_expected[:, :1], atol=1e-6, err_msg=f"Kernel evaluation fails for multiple first input vectors, but a single second input vector")
 
     def test_call_symmetric(self):
         # Test that calling the kernel alone results in a symmetric matrix
@@ -87,6 +115,46 @@ class PsuedoHuberKernelTest(unittest.TestCase):
         # Test that calling the kernel on two datasets results in an asymmetric matrix
         Kxy = self.kernel(self.x, self.y)
         np.testing.assert_allclose(Kxy, self.Kxy_expected, atol=1e-6, err_msg=f"Calling kernel on two datasets results in inaccurate asymmetric kernel matrix")
+
+    def test_gradient(self):
+        """Test evaluation of the kernel gradients"""
+        x_ad = np.squeeze(ad.InitializeAutoDiff(self.x[:, 2]))
+        # Test evaluating the kernel gradient with a single y-sample
+        k_ad = self.kernel.eval(x_ad, self.y[:, 0])
+        dk_ad = ad.ExtractGradient(k_ad)
+        dk = self.kernel.gradient(self.x[:, 2], self.y[:, 0])
+        np.testing.assert_allclose(dk, dk_ad, atol=1e-6, err_msg=f"Gradient calculation does not match gradient calculated by autodiff for single y-vector")
+        # Test that we can evaluate the kernel gradient with multiple y-samples
+        k_ad = self.kernel.eval(x_ad, self.y)
+        dk_ad = ad.ExtractGradient(k_ad)
+        # Evaluate the gradient directly
+        dk = self.kernel.gradient(self.x[:, 2], self.y)
+        np.testing.assert_allclose(dk, dk_ad, atol=1e-6, err_msg=f"Gradient calculation does not match gradient calcuated by autodiff for multiple y-vectors")
+
+    def test_eval_autodiff(self):
+        """ Test evaluating the kernel when one of the inputs is an autodiff type"""
+        x_ad = ad.InitializeAutoDiff(self.x[:, 0])
+        # Test with a single sample y-point
+        Kxy_ad = self.kernel.eval(x_ad, self.y[:, 0])
+        np.testing.assert_allclose(ad.ExtractValue(Kxy_ad), self.Kxy_expected[:1, :1], atol=1e-6, err_msg=f"Kernel evaluation fails when first argument is autodiff, second argument is vector")
+        # Test with multiple input sample y-points
+        Kxy_ad = self.kernel.eval(x_ad, self.y)
+        np.testing.assert_allclose(ad.ExtractValue(Kxy_ad), self.Kxy_expected[:1,:], atol=1e-6, err_msg=f"Kernel evaluation fails when first argument is autodiff, second argument is array")
+
+    def test_gradient_autodiff(self):
+        """Test evaluating the kernel gradient when one of the inputs is an autodiff type"""
+        x_ad = ad.InitializeAutoDiff(self.x[:, 0])
+        # Test with a single y-point
+        dKxy_ad  =self.kernel.gradient(x_ad, self.y[:, 0])
+        dKxy = ad.ExtractValue(dKxy_ad)
+        gradKxy = self.kernel.eval(x_ad, self.y[:, 0])
+        np.testing.assert_allclose(dKxy, ad.ExtractGradient(gradKxy), atol=1e-6, err_msg=f"Evaluating gradient fails when first input is autodiff, second input is a single vector")
+        # Test with multiple y-points
+        dKxy_ad = self.kernel.gradient(x_ad, self.y)
+        dKxy = ad.ExtractValue(dKxy_ad)
+        gradKxy = self.kernel.eval(x_ad, self.y)
+        np.testing.assert_allclose(dKxy, ad.ExtractGradient(gradKxy), atol=1e-6, err_msg=f"Evaluating gradient fails when first input is autodiff, second input is multiple vectors")
+
 
 if __name__ == '__main__':
     unittest.main()
