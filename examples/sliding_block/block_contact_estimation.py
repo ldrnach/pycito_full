e05d7ed054f0af12fd94a6c7b0ddee58dba2d1f8
Luke Drnach <luke.drnach@gmail.com>
2022-03-29 17:27:40 -0400

    minor changes

--- examples/sliding_block/block_contact_estimation.py
+++ examples/sliding_block/block_contact_estimation.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+from collections import defaultdict
 from pycito.controller import contactestimator as ce
 from pycito.systems.block.block import Block
 from pycito.systems.contactmodel import SemiparametricContactModel
@@ -20,9 +21,12 @@ def get_data(sourcepart):
     data = utils.load(utils.FindResource(file))
     return data
 
-def make_estimator(data):
+def make_estimator(sourcepart):
+    data = get_data(sourcepart)
     block = make_block()
-    traj = ce.ContactEstimationTrajectory(block,data['state'][:, 0])
+    traj = ce.ContactEstimationTrajectory(block, data['state'][:, 0])    
+    for t, x, u in zip(data['time'][1:], data['state'][:, 1:].T, data['control'][:, 1:].T):
+        traj.append_sample(t, x, u)
     estimator = ce.ContactModelEstimator(traj, horizon=5)
     estimator.useSnoptSolver()
     estimator.setSolverOptions({'Major feasibility tolerance': 1e-6,
@@ -48,16 +52,13 @@ def check_plotter(filepart):
     plotter = ce.ContactEstimationPlotter(traj)
     plotter.plot(show=True)
 
-
 def check_program_variables(estimator):
-    dvars = estimator.prog.decision_variables()
-    names = [dvar.get_name() for dvar in dvars]
-    print(names)
-    var_names = set([name.split('(')[0]] for name in names)
-    print(var_names)
+    estimator.create_estimator()
+    dvars = estimator.get_decision_variable_dictionary()
+    print(dvars.keys())
 
 
 if __name__ == '__main__':
-    check_plotter('flatterrain')
-    #estimator = make_estimator('flatterrain')
-    #check_program_variables(estimator)
+    #check_plotter('flatterrain')
+    estimator = make_estimator('flatterrain')
+    check_program_variables(estimator)
