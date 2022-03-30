6edcae01e2e9ca9a76f8704496160ed76ad1eca1
Luke Drnach <ldrnach3@gatech.edu>
2022-03-30 11:21:28 -0400

    check if solver is enabled

--- tutorials/optimization/solvercheck.py
+++ tutorials/optimization/solvercheck.py
@@ -1,6 +1,13 @@
 from pydrake.all import SnoptSolver, GurobiSolver, IpoptSolver, OsqpSolver
 
 print(f"SNOPT available: {SnoptSolver().available()}")
-print(f"Gurobi available: {GurobiSolver().available()}")
-print(f"IPOPT available: {IpoptSolver().available()}")
-print(f"OSQP available: {OsqpSolver().available()}")
+print(f"SNOPT enabled: {SnoptSolver().enabled()}")
+
+print(f"\nGurobi available: {GurobiSolver().available()}")
+print(f"\Gurobi enabled: {GurobiSolver().enabled()}")
+
+print(f"\nIPOPT available: {IpoptSolver().available()}")
+print(f"IPOPT enabled: {IpoptSolver().enabled()}")
+
+print(f"\nOSQP available: {OsqpSolver().available()}")
+print(f"OSQP enabled: {OsqpSolver().enabled()}")
