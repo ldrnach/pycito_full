e68210f2270a423be2bad1f45b2eeeba9b683390
Luke Drnach <ldrnach3@gatech.edu>
2022-04-04 18:02:14 -0400

    refactoring and tuning optimization

--- examples/sliding_block/block_contact_estimation.py
+++ examples/sliding_block/block_contact_estimation.py
@@ -5,14 +5,20 @@ from pycito.controller import contactestimator as ce
 from pycito.systems.block.block import Block
 from pycito.systems.contactmodel import SemiparametricContactModel
 import pycito.utilities as utils
+from pycito.controller.optimization import OptimizationLogger
 
 SOURCEBASE = os.path.join('examples','sliding_block','simulations')
 SOURCEFILE = os.path.join('mpc', 'simdata.pkl')
+SAVEDIR = os.path.join('examples','sliding_block','estimation_SNRtests','SNR100000')
+FIGURENAME = 'EstimationResults.png'
+LOGFIGURE = 'SolverLogs.png'
+LOGGINGNAME = 'solutionlogs.pkl'
+TRAJNAME = 'estimatedtrajectory.pkl'
 
 def make_block():
     block = Block()
     block.Finalize()
-    block.terrain = SemiparametricContactModel.FlatSurfaceWithRBFKernel(friction = 0.5, length_scale = 0.1)
+    block.terrain = SemiparametricContactModel.FlatSurfaceWithRBFKernel(friction = 0.5, length_scale = 0.1, reg=0.01)
     return block
 
 def get_data(sourcepart):
@@ -21,13 +27,13 @@ def get_data(sourcepart):
     data = utils.load(utils.FindResource(file))
     return data
 
-def make_estimator(sourcepart):
-    data = get_data(sourcepart)
+def make_estimator(data):
     block = make_block()
     traj = ce.ContactEstimationTrajectory(block, data['state'][:, 0])    
-    for t, x, u in zip(data['time'][1:], data['state'][:, 1:].T, data['control'][:, 1:].T):
-        traj.append_sample(t, x, u)
     estimator = ce.ContactModelEstimator(traj, horizon=5)
+    # Set the costs appropriately
+    estimator.forcecost = 0.01
+    estimator.relaxedcost = 1000
     estimator.useSnoptSolver()
     estimator.setSolverOptions({'Major feasibility tolerance': 1e-6,
                                 'Major optimality tolerance': 1e-6})
@@ -36,29 +42,40 @@ def make_estimator(sourcepart):
 def run_estimation(filepart):
     data = get_data(filepart)
     estimator = make_estimator(data)
-    
+    logger = OptimizationLogger(estimator)
+    # Run the initial point
+    estimator.create_estimator()
+    print(f'Contact estimation at timestep {data["time"][0]:.2f}:', end='', flush=True)
+    result = estimator.solve()
+    print(f" Solved successfully? {result.is_success()}")
+    estimator.update_trajectory(data['time'][0], result)
+    logger.log(result)
     # Loop over each part of the contact estimation problem
     for t, x, u in zip(data['time'][1:], data['state'][:, 1:].T, data['control'][:, 1:].T):
+        print(f'Contact estimation at timestep {t:.2f}:', end='', flush=True)
         estimator.traj.append_sample(t, x, u)
         estimator.create_estimator()
         result = estimator.solve()
+        print(f" Solved successfully? {result.is_success()}")
+        estimator.update_trajectory(t, result)
+        logger.log(result)
+    # Save the contact estimation trajectory
+    estimator.traj.saveContactTrajectory(os.path.join(SAVEDIR, filepart, TRAJNAME))
+    # Plot the overall results
+    plotter = ce.ContactEstimationPlotter(estimator.traj)
+    plotter.plot(show=False, savename=os.path.join(SAVEDIR, filepart, FIGURENAME))
+    logger.plot(show=False, savename=os.path.join(SAVEDIR, filepart, LOGFIGURE))
+    logger.save(filename = os.path.join(SAVEDIR, filepart, LOGGINGNAME))
 
-def check_plotter(filepart):
-    data = get_data(filepart)
-    block = make_block()
-    traj = ce.ContactEstimationTrajectory(block, data['state'][:, 0])
-    for t, x, u in zip(data['time'][1:], data['state'][:, 1:].T, data['control'][:, 1:].T):
-        traj.append_sample(t, x, u)
-    plotter = ce.ContactEstimationPlotter(traj)
-    plotter.plot(show=True)
+def replot_logs(filepart):
+    logger = OptimizationLogger.load(os.path.join(SAVEDIR, filepart, LOGGINGNAME))
+    logger.plot(show=True)
 
-def check_program_variables(estimator):
-    estimator.create_estimator()
-    dvars = estimator.get_decision_variable_dictionary()
-    print(dvars.keys())
-
+def open_logs(filepart):
+    logger = OptimizationLogger.load(os.path.join(SAVEDIR, filepart, LOGGINGNAME))
+    print("Hello!")
 
 if __name__ == '__main__':
-    #check_plotter('flatterrain')
-    estimator = make_estimator('flatterrain')
-    check_program_variables(estimator)
+    run_estimation('steppedterrain')
+    #replot_logs('flatterrain')
+    #open_logs('flatterrain')
