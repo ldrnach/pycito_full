bd3958596822ddbe668bbff534e6bbf696234837
Luke Drnach <luke.drnach@gmail.com>
2022-04-29 16:49:26 -0400

    small fixes to simulator

--- pycito/systems/simulator.py
+++ pycito/systems/simulator.py
@@ -4,19 +4,30 @@ Contact Simulation
 Luke Drnach
 February 8, 2022
 """
-
+#TODO: Determine a good method for integrating the contact dynamics
 import numpy as np
 import pycito.controller.mpc as mpc
+from pycito.systems.integrators import ContactDynamicsIntegrator
 
 class Simulator():
     def __init__(self, plant, controller):
         self.plant = plant
         self.controller = controller
-        self._timestep = 0.01
+        self._timestep = np.array([0.01])
+        self.integrator = ContactDynamicsIntegrator.ImplicitMidpointIntegrator(plant)
 
     @staticmethod
     def Uncontrolled(plant):
         return Simulator(plant, mpc.NullController(plant))
+
+    def useImplicitEuler(self):
+        self.integrator = ContactDynamicsIntegrator.ImplicitEulerIntegrator(self.plant)
+
+    def useSemiImplicitEuler(self):
+        self.integrator = ContactDynamicsIntegrator.SemiImplicitEulerIntegrator(self.plant)
+
+    def useImplicitMidpoint(self):
+        self.integrator = ContactDynamicsIntegrator.ImplicitMidpointIntegrator(self.plant)
 
     def simulate(self, initial_state, duration):
         """Run the simulation"""
@@ -51,8 +62,11 @@ class Simulator():
                 status = False
                 break            
         # Return the simulation values
-        return time, state, control, force, status
-
+        if status:
+            return time, state, control, force, status
+        else:
+            return time[:n-1], state[:, n-1], control[:, n-1], force[:, n-1], status
+        
     @property
     def timestep(self):
         return self._timestep
