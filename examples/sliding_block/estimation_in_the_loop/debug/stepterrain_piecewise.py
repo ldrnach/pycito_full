8c6a14d5d0ca58de8da2034421ece6f9d84f550d
Luke Drnach <luke.drnach@gmail.com>
2022-05-18 11:31:27 -0400

    added more comparisons to piecewise example

--- examples/sliding_block/estimation_in_the_loop/debug/stepterrain_piecewise.py
+++ examples/sliding_block/estimation_in_the_loop/debug/stepterrain_piecewise.py
@@ -1,5 +1,5 @@
 
-import os, copy
+import os, copy, time
 import numpy as np
 import matplotlib.pyplot as plt
 from pycito.systems.block.block import Block
@@ -15,7 +15,7 @@ SOURCE = os.path.join("examples","sliding_block","estimation_in_the_loop","stept
 REFDATA = 'campcsim.pkl'
 ESTRAJ = 'estimatedtrajectory.pkl'
 LOGDATA = os.path.join('campc_logs','EstimationLogs.pkl')
-INDEX = 80
+INDEX = 140
 GLOBAL_MODEL = False
 
 def make_stepterrain_model():
@@ -50,6 +50,8 @@ ns_contact = cm.SemiparametricContactModel(
 )
 # Update the contact estimation trajectory
 # Store the existing model, distance, and friction coefficients
+print(f"Updating contact model\t", end="", flush=True)
+start = time.perf_counter()
 subtraj = estraj.subset(0, INDEX)
 for k, cpt in enumerate(subtraj._contactpoints):
     dk = sp_contact.surface_kernel(cpt, cpts).dot(dweights)
@@ -60,23 +62,42 @@ for k, cpt in enumerate(subtraj._contactpoints):
     subtraj._friction_cstr[k] += df
 
 subtraj.contact_model = ns_contact
-
+print(f"Elapsed: {time.perf_counter() - start:0.2e}", end="\n", flush=True)
+print(f"Solving global contact estimation\t", end="", flush=True)
+start = time.perf_counter()
 rectifier = EstimatedContactModelRectifier(subtraj, surf_max = 2, fric_max = 2)
 rectifier.useSnoptSolver()
 rectifier.setSolverOptions({'Major Feasibility Tolerance': 1e-4, 
                             'Major Optimality Tolerance': 1e-4})
 pw_model = rectifier.solve_global_model_with_ambiguity()
 lb_model = pw_model.lower_bound
+dweights = pw_model.surface._kernel_weights
+fweights = pw_model.friction._kernel_weights
+print(f"Elapsed: {time.perf_counter() - start:0.2e}", end="\n", flush=True)
+
 # Plot the results
 terrain_start = np.array([0, 0, 0])
 terrain_end = np.array([6, 0, 0])
 terrain_samples = np.linspace(terrain_start, terrain_end, 701).T
+print(f"Evaluating true terrain\t", end="", flush=True)
+start = time.perf_counter()
 true_terrain = truemodel.terrain.find_surface_zaxis_zeros(terrain_samples)
+print(f"Elapsed: {time.perf_counter() - start:0.2e}", end="\n", flush=True)
+print(f"Evaluating linear terrain\t", end="", flush=True)
+start = time.perf_counter()
 sp_terrain = sp_contact.find_surface_zaxis_zeros(terrain_samples)
+print(f"Elapsed: {time.perf_counter() - start:0.2e}", end="\n", flush=True)
+print(f"Evaluating piecewise terrain\t", end="", flush=True)
+start = time.perf_counter()
 pw_terrain = pw_model.find_surface_zaxis_zeros(terrain_samples)
+print(f"Elapsed: {time.perf_counter() - start:0.2e}", end="\n", flush=True)
+print(f"Evaluating upper_bound terrain\t", end="", flush=True)
+start = time.perf_counter()
 ub_terrain = lb_model.find_surface_zaxis_zeros(terrain_samples)
 cpts_all = np.concatenate(estraj.get_contacts(0, INDEX), axis=1)
-
+print(f"Elapsed: {time.perf_counter() - start:0.2e}")
+print(f"Elapsed: {time.perf_counter() - start:0.2e}", end="\n", flush=True)
+print(f"Making plots")
 fig, axs = plt.subplots(2,1)
 axs[0].plot(true_terrain[0,:], true_terrain[2,:], 'g-', linewidth=1.5, label='Actual')
 axs[0].plot(cpts_all[0,:], cpts_all[2,:], '*', label='Contacts')
@@ -87,5 +108,26 @@ axs[0].plot(ub_terrain[0,:], ub_terrain[2,:], 'y-', linewidth=1.5, label='UpperB
 axs[0].set_ylabel('Terrain (m)')
 axs[0].set_xlabel('Position (m)')
 
+pw_model.compress(atol=1e-3)
+axs[1].plot(true_terrain[0,:], true_terrain[2,:], linewidth=1.5, label='Actual')
+axs[1].plot(pw_terrain[0,:], pw_terrain[2,:], linewidth=1.5, label='Full Model')
+print(f"Evaluating compressed terrain\t", end="", flush=True)
+start = time.perf_counter()
+cw_terrain = pw_model.find_surface_zaxis_zeros(terrain_samples)
+print(f"Elapsed: {time.perf_counter() - start:0.2e}", end="\n", flush=True)
+axs[1].plot(cw_terrain[0,:], cw_terrain[2,:], linewidth=1.5, label='Compressed Model')
+
+print(f"Compressed model has {pw_model.surface._kernel_weights.shape[0]}/{INDEX} surface samples and {pw_model.friction._kernel_weights.shape[0]}/{INDEX} friction samples ")
+
 axs[0].legend(frameon=False)
+axs[1].legend(frameon=False)
+plt.show()
+
+fig, axs = plt.subplots(2,1)
+axs[0].hist(dweights, bins=20)
+axs[0].set_xlabel("Distance Weights")
+axs[0].set_ylabel('Frequency')
+axs[1].hist(fweights, bins=20)
+axs[1].set_xlabel('Friction weights')
+axs[1].set_ylabel('Frequency')
 plt.show()
